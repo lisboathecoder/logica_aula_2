27b8e5411e92cddd2d4afdf0b9ab7cc2fdd73e29
Gustavo Lisboa <gustavo.lisboa2009@gmail>
2025-03-07 11:25:25 -0300

    commit

---
+++ logica_aula_2/atividade_6/exercico_2.js
@@ -0,0 +1,20 @@
+let nota = 75
+
+if (nota >= 90) {
+    console.log(`Consegue realizar tarefas e Ajudar`)
+}
+else if(nota >= 79) {
+    console.log(`Consegue realizar tarefas com autonomia!`)
+}
+else if(nota >= 70) {
+    console.log(`Consegue realizar tarefas com exemplos!`)
+}
+else if(nota >= 50) {
+    console.log(`Consegue realizar tarefas com ajuda!`)
+}
+else {
+    console.log(`Reprovado.`)
+}
+
+// Gustavo Teixeira Lisboa
+// 07/03/2025 11:30
