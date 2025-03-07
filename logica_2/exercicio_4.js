a60c9de2f1e74c2713a0de827b64d31c6af34278
Gustavo Lisboa <gustavo.lisboa2009@gmail>
2025-03-07 09:46:55 -0300

    commit

--- logica_2/exercicio_4.js
+++ logica_2/exercicio_4.js
@@ -3,7 +3,8 @@ let nota2 = 7
 let nota3 = 5
 let nota4 = 10
 let nota5 = 10
-let soma = nota1 + nota2 + nota3 + nota4 + nota5
+let nota6 = 9
+let soma = nota1 + nota2 + nota3 + nota4 + nota5 + nota6
 let resultado = soma / 6 
 
 console.log("A média de notas é igual a" , resultado)
