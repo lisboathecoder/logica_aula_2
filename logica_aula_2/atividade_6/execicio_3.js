731ea3c2871baee31ee66fd4e4c1149121009f75
Gustavo Lisboa <gustavo.lisboa2009@gmail>
2025-03-07 11:35:16 -0300

    commit

---
+++ logica_aula_2/atividade_6/execicio_3.js
@@ -0,0 +1,10 @@
+let valor = 23
+if (valor%2 == 0){
+    console.log(`O número ${valor} recebido é par!`)
+}
+else {
+    console.log(`O número ${valor} recebido é ímpar`)
+}
+
+// Gustavo Teixeira Lisboa
+// 07/03/2025 11:35
