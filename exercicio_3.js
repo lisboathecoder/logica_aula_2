193fdc6bbba4f7eea97ff6c2b293e1f07e2a8afd
Gustavo Lisboa <gustavo.lisboa2009@gmail>
2025-03-24 07:57:04 -0300

    commit

--- exercicio_3.js
+++
@@ -1,8 +0,0 @@
-let saldo = 1000
-saldo += 500
-saldo -= 200
-saldo *= 1.05
-console.log (`O saldo é ${saldo.toFixed(2)}R$`)
-
-// Gustavo Teixeira Lisboa
-// 07/03/2025 09:40
--- exercicio_5.js
+++
@@ -1,8 +0,0 @@
-let valor = 10
-
-let resultado = (valor % 2 === 0) ? "par" : "impar"
-
-console.log(`O número ${valor} é ${resultado}.`)
-
-// Gustavo Teixeira Lisboa
-// 07/03/2025 09:40
