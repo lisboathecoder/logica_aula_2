05b6e1414892a2acf85623bc66d41cf03fc9c6eb
Gustavo Lisboa <gustavo.lisboa2009@gmail>
2025-03-24 09:31:52 -0300

    commit

---
+++ exercicio_1.js
@@ -0,0 +1,6 @@
+let pontos = 50 + 10
+
+console.log(pontos *2)   
+
+// Gustavo Teixeira Lisboa
+// 07/03/2025 09:40
---
+++ exercicio_5.js
@@ -0,0 +1,8 @@
+let valor = 10
+
+let resultado = (valor % 2 === 0) ? "par" : "impar"
+
+console.log(`O número ${valor} é ${resultado}.`)
+
+// Gustavo Teixeira Lisboa
+// 07/03/2025 09:40
